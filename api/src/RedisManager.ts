5eb97f69bf4181d9e038985c05987d2bddd25e64
shweta <shwetasinghh9416@gmail.com>
2024-09-21 16:12:07 +0530

    redis manager created

---
+++ api/src/RedisManager.ts
@@ -0,0 +1,23 @@
+import { createClient, RedisClientType } from "redis";
+
+export class RedisManager {
+  private static instance: RedisManager;
+  private redisClient: RedisClientType;
+
+  private constructor() {
+    this.redisClient = createClient();
+    this.redisClient.connect();
+  }
+
+  public static getInstance(): RedisManager {
+    if (RedisManager.instance) 
+        return RedisManager.instance;
+
+    return (RedisManager.instance = new RedisManager());
+  }
+
+  public async pushToQueue({message, to, service, priority}:{message: string, to: string, service:string, priority: number}){
+    const channel = `${service}-${priority}`;
+    await this.redisClient.lPush(channel, JSON.stringify({message, to}))
+  }
+}
